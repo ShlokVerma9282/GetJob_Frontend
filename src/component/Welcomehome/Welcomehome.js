f75d1b6fce55d293462d5b8a4fee01f58e7d4075
Shlok Verma <87667355+SoraUchiha@users.noreply.github.com>
2023-08-04 00:52:36 +0530

    first commit

---
+++ src/component/Welcomehome/Welcomehome.js
@@ -0,0 +1,26 @@
+import { Grid, Typography } from "@material-ui/core";
+import { Link } from 'react-router-dom';
+import React from "react";
+import './Welcomehome.css';
+
+/*The common Landing Page where all the different services define*/
+const Welcomehome = () => (
+    <React.Fragment>
+        <div className="home-container">
+            <div className="home-content">
+                <h1 className="home-heading">GetJob With Portfolio Maker</h1>
+                <p className="home-description">
+                    Millions of people are searching for jobs, salary information, company
+                    reviews. Find the job that fits your abilities and potential.
+                </p>
+                <p className="home-description">Get your dream job via GetJob</p>
+                <Link to="/jobs">
+                    <button type="button" className="shop-now-button">
+                        Project Demo
+                    </button>
+                </Link>
+            </div>
+        </div>
+    </React.Fragment>
+)
+export default Welcomehome
